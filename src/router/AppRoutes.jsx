3889e6cc4c8d0df50aeee04379f196090a22762a
VrutikPatel100 <tejasshah@gmail.com>
2026-04-13 20:31:03 +0530

    login

---
+++ src/router/AppRoutes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Login } from "../common/Login"
+
+const AppRoutes = ()=>{
+    const router = createBrowserRouter([
+        {
+        path:"/login",
+        element:<Login/>
+        },
+         // {
+        //     path:"/signup",
+        //     element:<Signup/>
+        // }
+    ])
+     return <RouterProvider router={router} />
+}
+export default AppRoutes;

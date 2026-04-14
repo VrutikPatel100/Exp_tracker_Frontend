e818b8cef37ce88e8426eaeee0ed27dfe49de88d
VrutikPatel100 <tejasshah@gmail.com>
2026-04-14 14:55:28 +0530

    add category

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,17 +1,42 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Login } from "../common/Login"
+import { UserNavbar } from "../user/UserNavbar"
+import { ExpenseDashboard } from "../user/ExpenseDashboard"
+import { AddCategory } from "../user/AddCategory"
+import { GetMyCategories } from "../user/GetMyCategories"
 
 const AppRoutes = ()=>{
+
+
     const router = createBrowserRouter([
         {
-        path:"/login",
-        element:<Login/>
+            path:"/login",
+            element:<Login/>
         },
-         // {
+        {
+            path:"",
+            element:<UserNavbar/>,
+            children:[
+                {
+                    path:"",
+                    element:<ExpenseDashboard/>
+                },
+                {
+                    path:"add-category",
+                    element:<AddCategory/>
+                },{
+                    path:"my-categories",
+                    element:<GetMyCategories/>
+                }
+            ]
+        }
+        // {
         //     path:"/signup",
         //     element:<Signup/>
         // }
     ])
-     return <RouterProvider router={router} />
+
+    return <RouterProvider router={router} />
+
 }
 export default AppRoutes;
